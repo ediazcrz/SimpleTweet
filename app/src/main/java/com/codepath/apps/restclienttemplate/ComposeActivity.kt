092bd3588e85807b7a58e874d9589c9d25346927
Ernesto Diaz <ediaz188@fiu.edu>
2021-02-14 16:58:44 -0500

    moved text color change to afterTextChanged

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -35,7 +35,6 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable) {}
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 // Fires right as the text is being changed (even supplies the range of text)
@@ -48,8 +47,10 @@ class ComposeActivity : AppCompatActivity() {
                     btnTweet.isEnabled = true
                     tvCount.setTextColor(defaultTextColor)
                 }
-
-                tvCount.text = "${MAX_TWEET_LENGTH - count}"
+            }
+            
+            override fun afterTextChanged(s: Editable) {
+                tvCount.text = "${MAX_TWEET_LENGTH - s.length}"
             }
         })
 
